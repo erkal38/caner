a6623698abc09ff0278ac74147d5206594e94946
erkal38 <nalruvceysem1984@gmail.com>
2020-07-29 22:01:07 +0300

    erkal

---
+++ Review/src/selfStudy/ArrayLis.java
@@ -0,0 +1,16 @@
+package selfStudy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayLis {
+
+	public static void main(String[] args) {
+        List<Integer> ahmet=new ArrayList<Integer>();
+        ahmet.add(5);
+        System.out.println(5+2);
+        
+		
+	}
+
+}
